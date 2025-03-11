eef3425d7a35d94889490e597ba8d86ace8f0634
Rahuls-workplace <rahulpsndit3@gmail.com>
2025-03-11 20:38:08 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
+import About from "./pages/About";
 
 function App() {
     return (
-        <div>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                </Routes>
-            </Router>
-        </div>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
     );
 }
 
